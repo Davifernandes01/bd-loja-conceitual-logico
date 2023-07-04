e31ff94b74a7d544d1e2e7ce5444df4353bf48c2
davi <davifernadesr@gmail.com>
2023-07-04 10:56:42 -0300

    atualizando a tabela Cliente

--- lojA/Cliente.sql
+++ lojA/Cliente.sql
@@ -2,9 +2,11 @@ create table Cliente(
 
   id_cliente int not null primary KEY IDENTITY(1,1),
   nome varchar(15) not null,
-  sobrenome varchar(15) not null,
+  sobrenome varchar(15) not null, 
+  sexo char(1) not null CHECK (sexo in ('M', 'F')),
   data_nascimento date not null,
   cpf varchar(14) not null,
   email varchar(50) not null
 
 )
+
