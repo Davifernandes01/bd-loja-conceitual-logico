5464d06b1b228fc1d2ac3e863c6f6757cd4966f5
davi <davifernadesr@gmail.com>
2023-07-05 18:48:00 -0300

    tabela de associação entre clientes e produto

---
+++ lojA/Cliente_produto.sql
@@ -0,0 +1,12 @@
+create table Cliente_produto(
+  id_cliente_produto int not null primary key IDENTITY(1,1),
+  id_cliente int not null,
+  id_produto int not null,
+  quantidade TINYINT not NULL
+);
+
+alter TABLE Cliente_produto
+add CONSTRAINT fk_cliente_produto_cliente FOREIGN key(id_cliente) REFERENCES Cliente(id_cliente)
+
+alter TABLE Cliente_produto
+add CONSTRAINT fk_cliente_produto_produto FOREIGN key(id_produto) REFERENCES Produto(id_produto)
