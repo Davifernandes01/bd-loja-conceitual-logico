95608a578783bef80fa5c290f70e46b69ae800a7
davi <davifernadesr@gmail.com>
2023-07-06 08:33:49 -0300

    inserção dos status dos pedidos

---
+++ lojA/inserção_status_pedidos.sql
@@ -0,0 +1,15 @@
+insert into Status_pedido(
+  descricao
+)VALUES('aprovado')
+
+insert into Status_pedido(
+  descricao
+)VALUES('não aprovado')
+
+insert into Status_pedido(
+  descricao
+)VALUES('em analise')
+
+insert into Status_pedido(
+  descricao
+)VALUES('cancelado')
