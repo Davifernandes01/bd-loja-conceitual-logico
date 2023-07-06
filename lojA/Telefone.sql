39b1780538168d06676b1279b2aa8501be74db35
davi <davifernadesr@gmail.com>
2023-07-06 08:11:00 -0300

    adicionado no atributo numero, o unique, para na ter repetição

--- lojA/Telefone.sql
+++ lojA/Telefone.sql
@@ -10,3 +10,14 @@ CREATE table Telefone(
 
 alter table Telefone
 add CONSTRAINT fk_cliente_telefone FOREIGN key(id_cliente) REFERENCES Cliente(id_cliente)
+
+ALTER TABLE Telefone
+add CONSTRAINT un_numero UNIQUE(numero)
+
+/*
+insert into Telefone(
+  id_cliente, numero, dd
+)values(1,'98733098', '34')
+
+select * from Telefone
+*/
