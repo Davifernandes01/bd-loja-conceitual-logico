921ac35235c20c0e1e168ad4f1a55df6687f3916
davi <davifernadesr@gmail.com>
2023-07-06 08:45:40 -0300

    corrigindo o atributo data_insercao

--- lojA/Carrinho.sql
+++ lojA/Carrinho.sql
@@ -1,6 +1,9 @@
 CREATE table Carrinho(
 
   id_carrinho int not NULL PRIMARY key IDENTITY(1,1),
-  data_inserção date not null,
+  data_insercao date not null,
 
 );
+
+alter TABLE Carrinho
+add  data_insercao date not null
