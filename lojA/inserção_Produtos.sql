e7e1074009a578624f0c210ad5d2659819c72fb0
davi <davifernadesr@gmail.com>
2023-07-06 08:29:16 -0300

    arquivo de inserção dos Produtos

---
+++ lojA/inserção_Produtos.sql
@@ -0,0 +1,100 @@
+
+-- Arquivo: insert_produtos.sql
+
+-- Inserção dos produtos
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Camiseta', 29.99, 'Camiseta de algodão branca');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Calça Jeans', 59.99, 'Calça jeans azul clara');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Sapato de Couro', 99.99, 'Sapato social de couro preto');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Relógio de Pulso', 199.99, 'Relógio de pulso analógico');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Óculos de Sol', 49.99, 'Óculos de sol com lentes polarizadas');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Bolsa de Couro', 79.99, 'Bolsa de couro marrom');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Celular', 499.99, 'Smartphone com câmera de alta resolução');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Tablet', 299.99, 'Tablet com tela sensível ao toque');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Notebook', 899.99, 'Notebook com processador Intel Core i5');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Monitor', 199.99, 'Monitor LED de 24 polegadas');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Teclado', 29.99, 'Teclado sem fio para computador');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Mouse', 19.99, 'Mouse óptico com fio');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Câmera Digital', 149.99, 'Câmera digital compacta');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Fone de Ouvido', 39.99, 'Fone de ouvido estéreo com microfone');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Impressora', 129.99, 'Impressora a jato de tinta colorida');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Caixa de Som Bluetooth', 69.99, 'Caixa de som portátil com conexão Bluetooth');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Carregador Portátil', 49.99, 'Carregador portátil para dispositivos móveis');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('DVD Player', 79.99, 'DVD player com suporte a diversos formatos');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Smart TV', 699.99, 'Smart TV LED de 50 polegadas');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Cadeira de Escritório', 149.99, 'Cadeira de escritório ergonômica em couro sintético');
+
+-- Arquivo: insert_produtos.sql
+
+-- Inserção dos produtos
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Camisa Polo', 39.99, 'Camisa polo de algodão');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Calça Social', 79.99, 'Calça social preta');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Tênis Esportivo', 89.99, 'Tênis esportivo para corrida');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Bermuda Jeans', 49.99, 'Bermuda jeans despojada');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Boné', 19.99, 'Boné com logo bordado');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Jaqueta de Couro', 149.99, 'Jaqueta de couro legítimo');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Mochila', 59.99, 'Mochila resistente para uso diário');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Ventilador de Mesa', 29.99, 'Ventilador de mesa com ajuste de velocidade');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Cafeteira', 49.99, 'Cafeteira elétrica programável');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Liquidificador', 39.99, 'Liquidificador com jarra de vidro');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Aspirador de Pó', 99.99, 'Aspirador de pó com filtro HEPA');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Ferro de Passar', 29.99, 'Ferro de passar roupas a vapor');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Panela de Pressão', 69.99, 'Panela de pressão de aço inoxidável');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Conjunto de Talheres', 19.99, 'Conjunto de talheres de aço inoxidável');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Tapete', 39.99, 'Tapete macio para sala');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Luminária de Mesa', 24.99, 'Luminária de mesa com base flexível');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Pendrive', 14.99, 'Pendrive de 16GB');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Caixa de Ferramentas', 49.99, 'Caixa de ferramentas com diversas peças');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Cadeira de Praia', 29.99, 'Cadeira de praia dobrável');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Guarda-chuva', 9.99, 'Guarda-chuva compacto');
+
+
+-- Arquivo: insert_produtos.sql
+
+-- Inserção dos produtos
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Vestido Floral', 59.99, 'Vestido estampado floral');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Sapato Social', 89.99, 'Sapato social de couro marrom');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Chapéu de Praia', 19.99, 'Chapéu de palha para proteção solar');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Shorts Esportivo', 29.99, 'Shorts esportivo de secagem rápida');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Blusa de Tricô', 39.99, 'Blusa de tricô com detalhes em renda');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Sandália de Salto', 79.99, 'Sandália de salto alto e tiras finas');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Cinto de Couro', 24.99, 'Cinto de couro marrom com fivela dourada');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Moletom', 49.99, 'Moletom com capuz e bolso canguru');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Escova de Dentes Elétrica', 39.99, 'Escova de dentes elétrica com temporizador');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Secador de Cabelo', 59.99, 'Secador de cabelo com controle de temperatura');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Fritadeira Elétrica', 99.99, 'Fritadeira elétrica sem óleo');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Forno Elétrico', 129.99, 'Forno elétrico com capacidade para 30 litros');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Cadeira de Jardim', 69.99, 'Cadeira de jardim dobrável em madeira');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Colchão de Casal', 249.99, 'Colchão de casal ortopédico');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Mesa de Centro', 79.99, 'Mesa de centro em vidro temperado');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Toalha de Banho', 19.99, 'Toalha de banho felpuda em algodão');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Câmera de Segurança', 89.99, 'Câmera de segurança com visão noturna');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Caixa de Som Bluetooth', 59.99, 'Caixa de som portátil à prova d agua');
+
+
+
+-- Arquivo: insert_produtos.sql
+
+-- Inserção dos produtos
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Máquina de Café', 79.99, 'Máquina de café expresso automática');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Ventilador de Teto', 149.99, 'Ventilador de teto com controle remoto');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Smartwatch', 199.99, 'Smartwatch com monitor cardíaco');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Batedeira Planetária', 129.99, 'Batedeira planetária com tigela de inox');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Torradeira', 39.99, 'Torradeira elétrica com múltiplas opções de temperatura');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Pipoqueira Elétrica', 49.99, 'Pipoqueira elétrica com tampa transparente');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Purificador de Água', 199.99, 'Purificador de água com filtro de carvão ativado');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Mala de Viagem', 89.99, 'Mala de viagem rígida com rodinhas');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Cadeado TSA', 14.99, 'Cadeado com sistema de abertura TSA');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Squeeze', 9.99, 'Squeeze de plástico para armazenamento de líquidos');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Monitor Gamer', 299.99, 'Monitor gamer de alta resolução e taxa de atualização');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Teclado Gaming', 79.99, 'Teclado mecânico para jogos');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Mousepad', 19.99, 'Mousepad com superfície de tecido');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Headset', 69.99, 'Headset com cancelamento de ruído e microfone retrátil');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Cadeira Ergonômica', 199.99, 'Cadeira de escritório ergonômica com ajustes');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Lâmpada LED', 9.99, 'Lâmpada LED de baixo consumo');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Carregador de Celular', 29.99, 'Carregador rápido para celular');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Kit de Ferramentas', 59.99, 'Kit de ferramentas com chaves de fenda e alicates');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Organizador de Gavetas', 14.99, 'Organizador de gavetas com divisórias ajustáveis');
+INSERT INTO Produto (nome, preco, descricao) VALUES ('Tapete de Yoga', 24.99, 'Tapete de yoga antiderrapante e confortável');
+
+
+SELECT * from Produto
