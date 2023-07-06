5ee93a25adc37dbc88f1fde501d7edaf7e3e9fe9
davi <davifernadesr@gmail.com>
2023-07-06 09:02:14 -0300

    inserindo dados na tabela de associação, cliente_endereco

---
+++ lojA/insercao_endereco_cliente.sql
@@ -0,0 +1,46 @@
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (1, 1);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (2, 2);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (3, 3);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (4, 4);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (5, 5);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (6, 6);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (7, 7);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (8, 8);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (9, 9);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (10, 10);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (11, 11);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (12, 12);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (13, 13);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (14, 14);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (15, 15);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (16, 16);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (17, 17);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (18, 18);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (19, 19);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (20, 20);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (21, 21);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (22, 22);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (23, 23);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (24, 24);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (25, 25);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (26, 21);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (27, 27);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (28, 1);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (29, 29);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (49, 1);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (50, 2);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (51, 3);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (52, 4);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (53, 5);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (54, 6);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (55, 30);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (19, 2);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (19, 26);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (54, 32);
+INSERT INTO Cliente_endereco (id_cliente, id_endereco) VALUES (1, 33)
+
+
+
+
+
+SELECT * from Cliente_Endereco
