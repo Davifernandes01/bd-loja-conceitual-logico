a6670556cef7c7c032f47d4839ca512033cbbeea
davi <davifernadesr@gmail.com>
2023-07-06 08:09:53 -0300

    arquivo para inserir telefones(possui alguns repetidos, mas na hora do insert, eles n adiciona)

---
+++ lojA/inserção_telefone.sql
@@ -0,0 +1,84 @@
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (1, '987654321', '11');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (2, '123456789', '11');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (3, '555555555', '21');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (4, '999999999', '21');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (5, '888888888', '31');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (6, '777777777', '31');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (7, '444444444', '41');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (8, '222222222', '41');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (9, '666666666', '51');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (10, '333333333', '51');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (11, '555555555', '61');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (12, '444444444', '61');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (13, '111111111', '71');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (14, '777777777', '71');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (15, '999999999', '81');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (16, '888888888', '81');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (17, '222222222', '91');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (18, '333333333', '91');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (19, '123456789', '11');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (20, '987654321', '11');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (21, '555555555', '21');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (22, '999999999', '21');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (23, '777777777', '31');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (24, '888888888', '31');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (25, '222222222', '41');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (26, '123456789', '11');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (27, '987654321', '11');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (28, '555555555', '21');
+
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (29, '999999999', '21');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (30, '777777777', '31');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (31, '888888888', '31');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (32, '222222222', '41');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (33, '123456789', '11');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (1, '987654321', '11');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (2, '123456789', '11');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (3, '555555555', '21');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (4, '999999999', '21');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (5, '888888888', '31');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (6, '777777777', '31');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (7, '444444444', '41');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (8, '222222222', '41');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (9, '666666666', '51');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (10, '333333333', '51');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (11, '555555555', '61');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (12, '444444444', '61');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (13, '111111111', '71');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (14, '777777777', '71');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (15, '999999999', '81');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (16, '888888888', '81');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (17, '222222222', '91');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (18, '333333333', '91');
+
+-- Exemplo de inserções na tabela Telefone
+-- Considere que a tabela Cliente já contém os registros necessários
+
+-- Inserções com números de telefone únicos
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (1, '987654321', '11');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (2, '123456789', '11');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (3, '555555555', '21');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (4, '999999999', '21');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (5, '888888888', '31');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (6, '777777777', '31');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (7, '444444444', '41');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (8, '222222222', '41');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (9, '666666666', '51');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (10, '333333333', '51');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (11, '555555555', '61');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (12, '444444444', '61');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (13, '111111111', '71');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (14, '777777777', '71');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (15, '999999999', '81');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (16, '888888888', '81');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (17, '222222222', '91');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (18, '333333333', '91');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (19, '444444443', '11');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (20, '555555552', '11');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (21, '666666661', '21');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (22, '777777770', '21');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (23, '888888889', '31');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (24, '999999998', '31');
+INSERT INTO Telefone (id_cliente, numero, dd) VALUES (25, '111111117', '41');
+
+
