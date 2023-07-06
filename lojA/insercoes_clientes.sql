92bd9f23a8e878c3789288fbc80feb28c5a6b42d
davi <davifernadesr@gmail.com>
2023-07-06 08:08:53 -0300

    arquivo para inserir clientes

---
+++ lojA/insercoes_clientes.sql
@@ -0,0 +1,173 @@
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('João', 'Silva', 'M', '1990-01-01', '12345678900', 'joao.silva@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Maria', 'Souza', 'F', '1985-05-10', '98765432100', 'maria.souza@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Pedro', 'Ferreira', 'M', '1992-11-15', '45678912300', 'pedro.ferreira@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Ana', 'Santos', 'F', '1994-07-20', '11122233344', 'ana.santos@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Lucas', 'Oliveira', 'M', '1988-03-05', '55566677788', 'lucas.oliveira@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Carla', 'Gomes', 'F', '1991-09-12', '99988877766', 'carla.gomes@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Fernando', 'Rodrigues', 'M', '1993-06-25', '33322211100', 'fernando.rodrigues@email.com');
+
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Camila', 'Almeida', 'F', '1987-02-18', '444.555.666-77', 'camila.almeida@email.com');
+
+-- Inserções adicionais
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Rafael', 'Costa', 'M', '1990-12-03', '123.456.789-01', 'rafael.costa@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Isabela', 'Fernandes', 'F', '1995-08-15', '987.654.321-01', 'isabela.fernandes@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Marcos', 'Pereira', 'M', '1986-04-21', '456.789.123-01', 'marcos.pereira@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Ana', 'Mendes', 'F', '1993-09-08', '111.222.333-45', 'ana.mendes@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Luiz', 'Ferreira', 'M', '1990-03-21', '555.666.777-89', 'luiz.ferreira@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Juliana', 'Rocha', 'F', '1988-11-14', '444.555.666-78', 'juliana.rocha@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('André', 'Oliveira', 'M', '1992-07-19', '333.222.111-01', 'andre.oliveira@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Carolina', 'Lima', 'F', '1987-05-25', '666.777.888-99', 'carolina.lima@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Roberto', 'Santos', 'M', '1991-12-12', '222.333.444-55', 'roberto.santos@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Tatiana', 'Gonçalves', 'F', '1993-04-02', '555.444.333-22', 'tatiana.goncalves@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Felipe', 'Cavalcante', 'M', '1989-09-07', '777.888.999-11', 'felipe.cavalcante@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Mariana', 'Pereira', 'F', '1994-02-17', '666.555.444-33', 'mariana.pereira@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Ricardo', 'Albuquerque', 'M', '1990-06-23', '888.999.111-22', 'ricardo.albuquerque@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Amanda', 'Vieira', 'F', '1986-10-05', '777.888.999-33', 'amanda.vieira@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Gustavo', 'Cunha', 'M', '1991-03-28', '999.888.777-44', 'gustavo.cunha@email.com');
+
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Larissa', 'Oliveira', 'F', '1989-08-15', '888.777.666-55', 'larissa.oliveira@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Marcelo', 'Fernandes', 'M', '1993-11-30', '666.555.444-22', 'marcelo.fernandes@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Vanessa', 'Pereira', 'F', '1988-05-25', '777.888.999-11', 'vanessa.pereira@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Fernando', 'Santos', 'M', '1992-12-18', '555.666.777-88', 'fernando.santos@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Carolina', 'Almeida', 'F', '1987-07-12', '444.555.666-77', 'carolina.almeida@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('André', 'Ferreira', 'M', '1991-09-20', '333.222.111-00', 'andre.ferreira@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Aline', 'Rocha', 'F', '1995-04-05', '222.333.444-55', 'aline.rocha@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Gustavo', 'Lima', 'M', '1990-02-28', '555.444.333-22', 'gustavo.lima@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Mariana', 'Cavalcante', 'F', '1986-09-07', '777.888.999-11', 'mariana.cavalcante@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Ricardo', 'Pereira', 'M', '1994-02-17', '666.555.444-33', 'ricardo.pereira@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Amanda', 'Albuquerque', 'F', '1990-06-23', '888.999.111-22', 'amanda.albuquerque@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Gustavo', 'Vieira', 'M', '1986-10-05','777.888.999-33', 'gustavo.vieira@email.com');
+
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Marcela', 'Cunha', 'F', '1991-03-28', '999.888.777-44', 'marcela.cunha@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Rafaela', 'Santos', 'F', '1989-08-15', '888.777.666-55', 'rafaela.santos@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Gustavo', 'Fernandes', 'M', '1993-11-30', '666.555.444-22', 'gustavo.fernandes@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Vanessa', 'Pereira', 'F', '1988-05-25', '777.888.999-11', 'vanessa.pereira@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Fernando', 'Santos', 'M', '1992-12-18', '555.666.777-88', 'fernando.santos@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Carolina', 'Almeida', 'F', '1987-07-12', '444.555.666-77', 'carolina.almeida@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('André', 'Ferreira', 'M', '1991-09-20', '333.222.111-00', 'andre.ferreira@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Aline', 'Rocha', 'F', '1995-04-05', '222.333.444-55', 'aline.rocha@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Gustavo', 'Lima', 'M', '1990-02-28', '555.444.333-22', 'gustavo.lima@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Mariana', 'Cavalcante', 'F', '1986-09-07', '777.888.999-11', 'mariana.cavalcante@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Ricardo', 'Pereira', 'M', '1994-02-17', '666.555.444-33', 'ricardo.pereira@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Amanda', 'Albuquerque', 'F', '1990-06-23', '888.999.111-22', 'amanda.albuquerque@email.com');
+
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('João', 'Silva', 'M', '1990-01-01', '123.456.789-00', 'joao.silva@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Maria', 'Santos', 'F', '1992-02-02', '987.654.321-00', 'maria.santos@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Pedro', 'Ferreira', 'M', '1988-03-03', '654.987.321-00', 'pedro.ferreira@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Ana', 'Lima', 'F', '1995-04-04', '789.321.654-00', 'ana.lima@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Carlos', 'Oliveira', 'M', '1991-05-05', '321.654.987-00', 'carlos.oliveira@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Fernanda', 'Almeida', 'F', '1987-06-06', '987.321.654-00', 'fernanda.almeida@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('José', 'Sousa', 'M', '1993-07-07', '654.321.987-00', 'jose.sousa@email.com');
+
+INSERT INTO Cliente (nome, sobrenome, sexo, data_nascimento, cpf, email)
+VALUES ('Paula', 'Rocha', 'F', '1989-08-08', '789.654.321-00', 'paula.rocha@email.com');
+
+
+
+
