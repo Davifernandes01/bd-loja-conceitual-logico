7463ce22463e91a17ca815172bdea1ba6239d679
davi <davifernadesr@gmail.com>
2023-07-06 09:16:39 -0300

    inserindo dados na tabela de associação cliente_produto

---
+++ lojA/insercao_cliente_produto.sql
@@ -0,0 +1,116 @@
+-- Exemplos com id_cliente entre 1 e 29, id_produto entre 1 e 79
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (1, 1);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (2, 15);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (3, 45);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (4, 2);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (5, 33);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (6, 71);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (7, 12);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (8, 7);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (9, 56);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (10, 79);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (11, 18);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (12, 40);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (13, 3);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (14, 63);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (15, 27);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (16, 68);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (17, 23);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (18, 50);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (19, 8);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (20, 77);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (21, 19);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (22, 43);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (23, 4);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (24, 66);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (25, 30);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (26, 72);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (27, 13);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (28, 10);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (29, 59);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (1, 6);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (2, 25);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (3, 48);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (4, 9);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (5, 61);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (6, 16);
+
+-- Exemplos com id_cliente entre 49 e 55, id_produto entre 1 e 79
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (49, 32);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (50, 67);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (51, 22);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (52, 54);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (53, 11);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (54, 75);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (55, 36);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (49, 60);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (50, 24);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (51, 47);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (52, 5);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (53, 57);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (54, 14);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (55, 70);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (49, 17);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (50, 42);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (51, 2);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (52, 34);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (53, 69);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (54, 26);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (55, 64);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (49, 21);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (50, 44);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (51, 3);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (52, 35);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (53, 70);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (54, 27);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (55, 65);
+
+-- Exemplos com id_cliente entre 1 e 15, id_produto entre 1 e 79
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (1, 37);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (2, 58);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (3, 20);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (4, 53);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (5, 9);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (6, 31);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (7, 76);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (8, 38);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (9, 62);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (10, 28);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (11, 73);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (12, 39);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (13, 55);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (14, 78);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (15, 46);
+
+-- Exemplos com id_cliente entre 1 e 29, id_produto entre 1 e 79
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (16, 10);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (17, 52);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (18, 16);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (19, 44);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (20, 8);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (21, 30);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (22, 66);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (23, 13);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (24, 19);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (25, 43);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (26, 4);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (27, 63);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (28, 27);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (29, 71);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (5, 56);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (12, 77);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (3, 18);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (8, 40);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (9, 3);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (7, 68);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (13, 23);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (11, 50);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (6, 7);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (10, 79);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (2, 19);
+INSERT INTO Cliente_produto (id_cliente, id_produto) VALUES (1, 43);
+
+
+--rodei o script 2 vezes
+
+select * from Cliente_produto
